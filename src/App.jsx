0df37076729e886ccede208c20570553497ade65
Ayush Bardhani <ayushbardhani03119@gmail.com>
2025-07-17 19:33:20 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind is working! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
